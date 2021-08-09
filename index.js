4eb5fc5c3bdf58f6ceff1fb67300733bb1940908
白浅 <fenfen.wff@dtwave-inc.com>
2021-08-09 14:33:18 +0800

    feat: add shader module demo

--- index.js
+++ index.js
@@ -1,99 +1,88 @@
-import {polyfillContext} from '@luma.gl/gltools'
+import {AnimationLoop, Model} from '@luma.gl/engine';
+import {Buffer, clear} from '@luma.gl/webgl';
 
-const canvas = document.createElement('canvas')
-canvas.width = 800
-canvas.height = 600
-document.body.appendChild(canvas)
-
-const gl = polyfillContext(canvas.getContext('webgl'))
-gl.clearColor(0, 0, 0, 1)
-
-// shader 顶点着色器、片元着色器
-const vs = `
+const vs1 = `
   attribute vec2 position;
-  attribute vec3 color; 
-  attribute vec2 offset;
 
-  varying vec3 vColor;
-
-  void main(){
-    vColor = color;
-    gl_Position = vec4(position + offset, 0.0, 1.0);
+  void main() {
+    gl_Position = vec4(position - vec2(0.5, 0.0), 0.0, 1.0);
   }
-`
+`;
 
-const fs = `
-  precision highp float;
+const fs1 = `
+  uniform vec3 hsvColor;
 
-  varying vec3 vColor;
-  void main(){
-    gl_FragColor = vec4(vColor, 1.0);
+  void main() {
+    gl_FragColor = vec4(color_hsv2rgb(hsvColor), 1.0);
   }
-`
-const vShader = gl.createShader(gl.VERTEX_SHADER)
-gl.shaderSource(vShader, vs)
-gl.compileShader(vShader)
+`;
 
-const fShader = gl.createShader(gl.FRAGMENT_SHADER)
-gl.shaderSource(fShader, fs)
-gl.compileShader(fShader)
+const vs2 = `
+  attribute vec2 position;
 
-const program = gl.createProgram()
-gl.attachShader(program, vShader)
-gl.attachShader(program, fShader)
-gl.linkProgram(program)
+  void main() {
+    gl_Position = vec4(position + vec2(0.5, 0.0), 0.0, 1.0);
+  }
+`;
 
-const vertexArray = gl.createVertexArray()
-gl.bindVertexArray(vertexArray)
+const fs2 = `
+  uniform vec3 hsvColor;
 
-// buffers
-const positionBuffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
-gl.bufferData(
-  gl.ARRAY_BUFFER,
-  new Float32Array([-0.2, -0.2, 0.2, -0.2, 0.0, 0.2]),
-  gl.STATIC_DRAW
-)
+  void main() {
+    gl_FragColor = vec4(color_hsv2rgb(hsvColor) - 0.3, 1.0);
+  }
+`;
 
-const positionLocation = gl.getAttribLocation(program, 'position')
-gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0)
-gl.enableVertexAttribArray(positionLocation)
+// Taken from http://lolengine.net/blog/2013/07/27/rgb-to-hsv-in-glsl
+const colorModule = {
+  name: 'color',
+  fs: `
+    vec3 color_hsv2rgb(vec3 c) {
+      vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+      vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+      return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+    }
+  `
+};
 
-const colorBuffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
-gl.bufferData(
-  gl.ARRAY_BUFFER,
-  new Float32Array([
-    1.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0,
-  ]),
-  gl.STATIC_DRAW
-)
+const loop = new AnimationLoop({
+  onInitialize({gl}) {
+    const positionBuffer = new Buffer(gl, new Float32Array([-0.3, -0.5, 0.3, -0.5, 0.0, 0.5]));
 
-const colorLocation = gl.getAttribLocation(program, 'color')
-gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0)
-gl.vertexAttribDivisor(colorLocation, 1)
-gl.enableVertexAttribArray(colorLocation)
+    const model1 = new Model(gl, {
+      vs: vs1,
+      fs: fs1,
+      modules: [colorModule],
+      attributes: {
+        position: positionBuffer
+      },
+      uniforms: {
+        hsvColor: [0.7, 1.0, 1.0]
+      },
+      vertexCount: 3
+    });
 
-const offsetBuffer = gl.createBuffer()
-gl.bindBuffer(gl.ARRAY_BUFFER, offsetBuffer)
-gl.bufferData(
-  gl.ARRAY_BUFFER,
-  new Float32Array([0.5, 0.5, -0.5, 0.5, 0.5, -0.5, -0.5, -0.5]),
-  gl.STATIC_DRAW
-)
+    const model2 = new Model(gl, {
+      vs: vs2,
+      fs: fs2,
+      modules: [colorModule],
+      attributes: {
+        position: positionBuffer
+      },
+      uniforms: { 
+        hsvColor: [1.0, 1.0, 1.0]
+      },
+      vertexCount: 3
+    });
 
-const offsetLocation = gl.getAttribLocation(program, 'offset')
-gl.vertexAttribPointer(offsetLocation, 2, gl.FLOAT, false, 0, 0)
-gl.vertexAttribDivisor(offsetLocation, 1)
-gl.enableVertexAttribArray(offsetLocation)
+    return {model1, model2};
+  },
 
-gl.bindVertexArray(null)
+  onRender({gl, model1, model2}) {
+    clear(gl, {color: [0, 0, 0, 1]});
+    model1.draw();
+    model2.draw();
+  }
+});
 
-requestAnimationFrame(function draw() {
-  requestAnimationFrame(draw)
-
-  gl.clear(gl.COLOR_BUFFER_BIT)
-  gl.bindVertexArray(vertexArray)
-  gl.useProgram(program)
-  gl.drawArraysInstanced(gl.TRIANGLES, 0, 3, 4)
-})
+loop.start();
